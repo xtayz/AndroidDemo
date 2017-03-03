608161a57878f41c9912e379fd95d4603931f2bf
zhha <zhanghao.dev@gmail.com>
2017-03-03 15:21:29 +0800

    create first activity

---
+++ app/src/main/java/com/zhanghao/androiddemo/FirstActivity.java
@@ -0,0 +1,35 @@
+package com.zhanghao.androiddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_add:
+                Toast.makeText(this, "You clicked add!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "You clicked remove", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
