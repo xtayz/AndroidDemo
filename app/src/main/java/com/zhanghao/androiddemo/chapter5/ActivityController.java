5aa27a36008e0d414d01a93bed11f9fd18916564
Hao Zhang <zhanghao.dev@gmail.com>
2017-03-08 21:28:18 +0800

    add BroadcastBestPractice

---
+++ app/src/main/java/com/zhanghao/androiddemo/chapter5/ActivityController.java
@@ -0,0 +1,8 @@
+package com.zhanghao.androiddemo.chapter5;
+
+/**
+ * Created by haozhang on 2017/3/8.
+ */
+
+public class ActivityController {
+}
---
+++ app/src/main/java/com/zhanghao/androiddemo/chapter5/BaseActivity1.java
@@ -0,0 +1,10 @@
+package com.zhanghao.androiddemo.chapter5;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by haozhang on 2017/3/8.
+ */
+
+public class BaseActivity1 extends AppCompatActivity {
+}
