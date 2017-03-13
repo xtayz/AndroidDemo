e6cc765dd7802042d4dfa667fceaeeb460554dae
Hao Zhang <zhanghao.dev@gmail.com>
2017-03-13 22:12:31 +0800

    学习ContentProvider

---
+++ app/src/main/java/com/zhanghao/androiddemo/chapter7/MyProvider.java
@@ -0,0 +1,76 @@
+package com.zhanghao.androiddemo.chapter7;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by haozhang on 2017/3/13.
+ */
+
+public class MyProvider extends ContentProvider {
+
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM = 1;
+    public static final int TABLE2_DIR = 2;
+    public static final int TABLE2_ITEM = 3;
+
+    public static UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.zhanghao.androiddemo.provider", "table1", TABLE1_DIR);
+        uriMatcher.addURI("com.zhanghao.androiddemo.provider", "table1/#", TABLE1_ITEM);
+        uriMatcher.addURI("com.zhanghao.androiddemo.provider", "table2", TABLE2_DIR);
+        uriMatcher.addURI("com.zhanghao.androiddemo.provider", "table2/#", TABLE2_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                break;
+            case TABLE1_ITEM:
+                break;
+            case TABLE2_DIR:
+                break;
+            case TABLE2_ITEM:
+                break;
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
