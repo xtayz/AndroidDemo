495d02565a727fcfce6d8ee25dc40df7100c0458
zhha <zhanghao.dev@gmail.com>
2017-03-16 15:47:00 +0800

    add PlayVideoTest

--- app/src/main/java/com/zhanghao/androiddemo/chapter8/PlayVideoTest.java
+++ app/src/main/java/com/zhanghao/androiddemo/chapter8/PlayVideoTest.java
@@ -1,16 +1,90 @@
 package com.zhanghao.androiddemo.chapter8;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.zhanghao.androiddemo.R;
 import com.zhanghao.androiddemo.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class PlayVideoTest extends BaseActivity {
+
+    @BindView(R.id.video_view)
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video_test);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+        } else {
+            initVideoPath();
+        }
+    }
+
+    private void initVideoPath() {
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.movie));
+        AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initVideoPath();
+                } else {
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @OnClick({ R.id.play, R.id.pause, R.id.replay }) void onClickedButton(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                if (!videoView.isPlaying()) {
+                    videoView.start();
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()) {
+                    videoView.resume();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();
+        }
     }
 }
