3e163b8d775271104409a09cd9539c07538f92be
zhha <zhanghao.dev@gmail.com>
2017-03-30 17:06:45 +0800

    add RequestManager

---
+++ app/src/main/java/com/zhanghao/androiddemo/chapter10/RequestManager.java
@@ -0,0 +1,38 @@
+package com.zhanghao.androiddemo.chapter10;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+/**
+ * Created by wonderworld on 2017/3/30.
+ */
+
+public class RequestManager {
+
+    public static final String BASE_URL = "https://api.douban.com/";
+    public static final int DEFAULT_TIMEOUT = 5;
+    private Retrofit retrofit;
+
+    private RequestManager() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
+        retrofit = new Retrofit.Builder()
+                .client(builder.build())
+//                .addConverterFactory(ResponseConvertFactory.create())
+                .baseUrl(BASE_URL)
+                .build();
+    }
+
+    private static class SingletonHolder {
+        private static final RequestManager INSTANCE = new RequestManager();
+    }
+
+    public static RequestManager getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+
+}
